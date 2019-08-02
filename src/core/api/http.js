173e5d2ccec203a0c428e79f1e52619de00161a6
smallhuai <52706399+smallhuai@users.noreply.github.com>
2019-08-02 15:36:07 +0800

    Update http.js

--- src/core/api/http.js
+++ src/core/api/http.js
@@ -31,5 +31,23 @@ var instance = axios.create({
     }],
     responseType: 'json'
 })
+// 添加请求拦截器
+instance.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+     //config.headers.token=JSON.parse(localStorage.getItem("userInfo")).token;
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+instance.interceptors.response.use(function (response) {
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
 
 export default instance
